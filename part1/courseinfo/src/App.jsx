05d363db6163db3660fb5c602e036f7b883fa49b
Jasmine John <jsjohn026@gmail.com>
2025-03-12 14:39:11 -0500

    implemented setState hook

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,60 +1,100 @@
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
+// const Header = ({ course }) => {
+//   return (
+//     <h1>{course}</h1>
+//   )
+// }
 
-const Part = ({ parts, exercises }) => {
-  return (
-    <p>
-      {parts} {exercises}
-    </p>
-  )
-}
+// const Part = ({ parts, exercises }) => {
+//   return (
+//     <p>
+//       {parts} {exercises}
+//     </p>
+//   )
+// }
 
-const Content = ({ parts }) => {
-  return (
-    <div>
-      <Part parts={parts[0].name} exercises={parts[0].exercises} />
-      <Part parts={parts[1].name} exercises={parts[1].exercises} />
-      <Part parts={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  )
-}
+// const Content = ({ parts }) => {
+//   return (
+//     <div>
+//       <Part parts={parts[0].name} exercises={parts[0].exercises} />
+//       <Part parts={parts[1].name} exercises={parts[1].exercises} />
+//       <Part parts={parts[2].name} exercises={parts[2].exercises} />
+//     </div>
+//   )
+// }
 
-const Total = ({ parts }) => {
-  return (
-    <div>
-      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </div>
-  )
-}
+// const Total = ({ parts }) => {
+//   return (
+//     <div>
+//       Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const course = { 
+//     name: 'Half Stack application development',
+//     parts: [
+//     {
+//     name: 'Fundamentals of React',
+//     exercises: 10
+//     },
+//     {
+//       name: 'Using props to pass data',
+//       exercises: 7
+//     },
+//     {
+//       name: 'State of a component',
+//       exercises: 14
+//     }
+//   ]}
+
+//   return (
+//     <div>
+//       <Header course={course.name} />
+//       <Content parts={course.parts} />
+//       <Total parts={course.parts} />
+//     </div>
+//   )
+// }
+
+// export default App
+
+import { useState } from "react";
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const course = { 
-    name: 'Half Stack application development',
-    parts: [
-    {
-    name: 'Fundamentals of React',
-    exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]}
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+  
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={setToZero} text='zero'/>
+      <Button onClick={decreaseByOne} text='minus'/>
     </div>
   )
 }
 
-export default App
+export default App;
