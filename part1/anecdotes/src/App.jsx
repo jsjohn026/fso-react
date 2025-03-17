a7eac3379ac3c1813b0b04f8fb7598064dc7f190
Jasmine John <jsjohn026@gmail.com>
2025-03-17 12:04:56 -0500

    completed up to exercises 1.14 anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Anecdote = ({ text }) => {  
+  return (
+    <div>
+      {text}
+    </div>
+  )
+}
+
 const Button = ({ onClick, text }) => {
   return (
     <button onClick={onClick}>{text}</button>
@@ -20,26 +28,41 @@ const App = () => {
 
   const [selected, getSelected] = useState(0)
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [highest, setHighest] = useState(0)
+  const [topAnecdote, setTopAnecdote] = useState('')
 
   const handleNextClick = () => {
     getSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
   const handleVote = () => {
-    const copyOfVotes = { ...votes }
+    // increment votes for the selected anecdote
+    const copyOfVotes = [...votes]
     copyOfVotes[selected] += 1
     setVotes(copyOfVotes)
+    
+    // set top number of votes in state
+    const topVotes = copyOfVotes.toSorted().slice(-1)
+    setHighest(topVotes)
+
+    // set the most popular anecdote in state
+    const topVotesIndex = copyOfVotes.indexOf(Number(topVotes))
+    setTopAnecdote(anecdotes[topVotesIndex])
   }
 
   console.log(votes)
+
   return (
     <div>
-      {anecdotes[selected]}
-      <br />
-      has {votes[selected]} votes
-      <br />
-      <Button onClick={handleVote} text='vote'/>
-      <Button onClick={handleNextClick} text='next anecdote' />
+      <h2>Anecdote of the day</h2>
+        <Anecdote text={anecdotes[selected]}/>
+        has {votes[selected]} votes
+        <br />
+        <Button onClick={handleVote} text='vote'/>
+        <Button onClick={handleNextClick} text='next anecdote' />
+      <h2>Anecdote with most votes</h2>
+        <Anecdote text={topAnecdote}/>
+        has {highest} votes
     </div>
   )
 }
