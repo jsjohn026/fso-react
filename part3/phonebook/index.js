16c28c0df5741317d79ec84af391b093b544d716
Jasmine John <jsjohn026@gmail.com>
2025-04-05 11:49:33 -0500

    Add addition function to phonebook to complete exercise 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -58,6 +58,35 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  const maxId = persons.length > 0 
+    ? Math.max(...persons.map(n => Number(n.id))) 
+    : 0
+  
+  return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+  
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT)
-console.log(`Server running on port ${PORT}`);
+console.log(`Server running on port ${PORT}`)
