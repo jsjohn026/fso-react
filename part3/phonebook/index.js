2bd7784636e488eaa7cc59416cad4d2579f1a057
Jasmine John <jsjohn026@gmail.com>
2025-04-08 08:33:04 -0500

    deployed backend successfully to complete exercise 3.10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let persons = [
   { 
@@ -98,6 +100,6 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
