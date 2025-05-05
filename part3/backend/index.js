0b473e6b1fa430f4d4b97e5dee9d54c04d66ceae
Jasmine John <jsjohn026@gmail.com>
2025-05-05 03:17:48 -0500

    added data validation error to post method

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -45,9 +45,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
   if (!body.content) {
     return response.status(400).json({
       error: 'content missing'
@@ -59,13 +58,14 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  
   const { content, important } = request.body
 
   Note.findById(request.params.id)
@@ -95,6 +95,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
