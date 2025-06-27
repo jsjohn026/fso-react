aa82570b20fe81be83b6c5a9d682cd707a44aff8
Jasmine John <jsjohn026@gmail.com>
2025-06-27 10:56:55 -0500

    created tests for most likes to complete exercise 4.5)

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -2,6 +2,30 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const posts = [
+  {
+    "title": "Test title",
+    "author": "Tester Testerson",
+    "url": "www.testerson.com",
+    "likes": 4,
+    "id": "684b353aa862d930a9dae1a3"
+  },
+  {
+    "title": "Marginal Revolution",
+    "author": "Tyler Cowen",
+    "url": "https://marginalrevolution.com/",
+    "likes": 15,
+    "id": "685af07cc15e05e6df4ca91a"
+  },
+  {
+    "title": "Wait But Why",
+    "author": "Tim Urban",
+    "url": "https://waitbutwhy.com//",
+    "likes": 10,
+    "id": "685af38dc15e05e6df4ca91d"
+  }
+]
+
 test('dummy returns 1', () => {
   const blogs = []
   const result = listHelper.dummy(blogs)
@@ -17,46 +41,20 @@ describe('total likes', () => {
   })  
 
   test('when list has only one blog equals the likes of that', () => {
-    const posts = [
-      {
-        "title": "Test title",
-        "author": "Tester Testerson",
-        "url": "www.testerson.com",
-        "likes": 4,
-        "id": "684b353aa862d930a9dae1a3"
-      }
-    ]
-
-    const result = listHelper.totalLikes(posts)
+    const list = [posts[0]]
+    const result = listHelper.totalLikes(list)
     assert.strictEqual(result, 4)
   })
 
   test('of a bigger list is calculated right', () => {
-    const posts = [
-      {
-        "title": "Test title",
-        "author": "Tester Testerson",
-        "url": "www.testerson.com",
-        "likes": 4,
-        "id": "684b353aa862d930a9dae1a3"
-      },
-      {
-        "title": "Marginal Revolution",
-        "author": "Tyler Cowen",
-        "url": "https://marginalrevolution.com/",
-        "likes": 15,
-        "id": "685af07cc15e05e6df4ca91a"
-      },
-      {
-        "title": "Wait But Why",
-        "author": "Tim Urban",
-        "url": "https://waitbutwhy.com//",
-        "likes": 10,
-        "id": "685af38dc15e05e6df4ca91d"
-      }
-    ]
-
     const results = listHelper.totalLikes(posts)
     assert.strictEqual(results, 29)
+  })
+})
+
+describe('most likes', () => {
+  test('of a list of blogs', () => {
+    const results = listHelper.favoriteBlog(posts)
+    assert.strictEqual(results, posts[1])
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (posts) => {
   return posts.reduce((accumulator, post) => {return accumulator += post.likes}, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((previous, current) => {
+    return previous.likes > current.likes ? previous : current
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes, 
+  favoriteBlog
 }
