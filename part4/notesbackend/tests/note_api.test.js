77136f48da3678b25ad049bea4af33daa599c652
Jasmine John <jsjohn026@gmail.com>
2025-07-24 12:49:58 -0500

    add tests to supertest module

--- part4/notesbackend/tests/note_api.test.js
+++ part4/notesbackend/tests/note_api.test.js
@@ -4,6 +4,7 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const assert = require('node:assert')
 
 test('notes are returned as json', async () => {
   await api
@@ -14,4 +15,15 @@ test('notes are returned as json', async () => {
 
 after(async () => {
   await mongoose.connection.close()
+})
+
+test('all notes are returned', async() => {
+  const response = await api.get('/api/note')
+  assert.strictEqual(response.body.length, 2)
+})
+
+test('a specific note is within the returned notes', async() => {
+  const response = await api.get('api/notes')
+  const contents = response.body.map(e => e.content)
+  assert.strictEqual(contents.includes('HTML is easy'), true)
 })
