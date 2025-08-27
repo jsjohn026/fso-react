0d3039147672cfb0d30a45221918fa4ae98c9c92
Jasmine John <jsjohn026@gmail.com>
2025-08-27 09:55:51 -0500

    Optimized beforeEach function in note_api test for Part 4-5

--- part4/notesbackend/tests/note_api.test.js
+++ part4/notesbackend/tests/note_api.test.js
@@ -10,15 +10,11 @@ const api = supertest(app)
 
 beforeEach(async () => {
   await Note.deleteMany({})
-
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  await Note.insertMany(helper.initialNotes)
 })
 
 test('notes are returned as json', async () => {
+  console.log('entered test')
   await api
     .get('/api/notes')
     .expect(200)
